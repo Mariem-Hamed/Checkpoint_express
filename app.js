c25cdea3e6afd38c34040a21ee7331d5b144ebe8
Mariem-Hamed <mariem.hamed@enis.tn>
2023-03-09 17:26:39 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+//Define the routes
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "home.html"));
+});
+
+app.get("/services", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "Ourservices.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "Contact.html"));
+});
+
+//Middleware
+const workingHoursMiddleware = (req, res, next) => {
+  const date = new Date();
+  const dayOfWeek = date.getDay();
+  const hour = date.getHours();
+  if (dayOfWeek >= 1 && dayOfWeek <= 5 && hour >= 9 && hour <= 17) {
+    next();
+  } else {
+    res.send("Sorry, the website is only available during working hours");
+  }
+};
+
+app.use(workingHoursMiddleware);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
