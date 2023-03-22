34c1ad248f9d9a55acec517dffc9ded935ef636d
Mariem-Hamed <mariem.hamed@enis.tn>
2023-03-22 11:08:02 +0100

    second commit

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+// define Middleware
+const workingHoursMiddleware = (req, res, next) => {
+  const date = new Date();
+  const dayOfWeek = date.getDay();
+  const hour = date.getHours();
+  if (dayOfWeek >= 1 && dayOfWeek <= 5 && hour >= 9 && hour <= 17) {
+    next();
+  } else {
+    res.send("Sorry, the website is only available during working hours");
+  }
+};
+
+// apply Middleware
+app.use(workingHoursMiddleware);
+
 //Define the routes
 
 app.get("/", (req, res) => {
@@ -15,20 +30,6 @@ app.get("/services", (req, res) => {
 app.get("/contact", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "Contact.html"));
 });
-
-//Middleware
-const workingHoursMiddleware = (req, res, next) => {
-  const date = new Date();
-  const dayOfWeek = date.getDay();
-  const hour = date.getHours();
-  if (dayOfWeek >= 1 && dayOfWeek <= 5 && hour >= 9 && hour <= 17) {
-    next();
-  } else {
-    res.send("Sorry, the website is only available during working hours");
-  }
-};
-
-app.use(workingHoursMiddleware);
 
 const port = process.env.PORT || 5000;
 
